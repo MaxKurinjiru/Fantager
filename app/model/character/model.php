82d99e2cb8d4a6dbdc96db4e86ace34e70d7a248
Jakub Němec <kurinjiru@gmail.com>
2020-05-27 10:57:58 +0200

    Update model.php

--- app/model/character/model.php
+++ app/model/character/model.php
@@ -1,1 +1,36 @@
+<?php
 
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Nette;
+
+
+final class Character
+{
+  private $id;
+  private $race;
+  private $sex;
+  private $old;
+  private $lvl;
+  private $exp;
+  private $name;
+  
+  private $hp_max;
+  private $hp;
+  private $mp_max;
+  private $mp;
+  
+  private $stats = [];
+  private $masteries = [];
+  private $combat = [
+    "p_dmg" => 0,
+    "m_dmg" => 0,
+    "p_def" => 0,
+    "m_def" => 0,
+    "speed" => 0
+  ];
+  
+  
+}
